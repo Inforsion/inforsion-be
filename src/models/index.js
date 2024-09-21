7ab06c727803b98e5396193849f55e7fe4c91be0
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2024-09-21 23:54:28 +0900

    build: connection pool option 추가

--- src/models/index.js
+++ src/models/index.js
@@ -9,40 +9,45 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env]
 const db = {};
 
-let sequelize;
-sequelize = new Sequelize(
+const sequelize = new Sequelize(
     config.database,
     config.username,
     config.password,
     config,
     {
-      connectionTimeout: 60000,
-      retry: {
-        max: 10,
-        timeout: 5000,
-      }
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
+        },
+        connectionTimeout: 60000,
+        retry: {
+            max: 10,
+            timeout: 5000,
+        }
     }
 );
 
 fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (
+            file.indexOf('.') !== 0 &&
+            file !== basename &&
+            file.slice(-3) === '.js' &&
+            file.indexOf('.test.js') === -1
+        );
+    })
+    .forEach(file => {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
+    });
 
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
 db.sequelize = sequelize;

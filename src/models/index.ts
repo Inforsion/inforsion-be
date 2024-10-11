32333483ebac797ac660e1da8a2d63f759d19cd0
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2024-10-12 00:39:20 +0900

    fix: dialect 명시 안했던 오류 해결

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { Sequelize, DataTypes, Options } from 'sequelize';
 import process from 'process';
-import configure from '../config/config.js';
+import configure from '../config/config';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+
 const config = configure[env];
 
 interface DB {
@@ -36,6 +37,7 @@ const sequelize = new Sequelize(
       max: 10,
       timeout: 5000,
     },
+    dialect: 'mysql',
   } as Options
 );
 
